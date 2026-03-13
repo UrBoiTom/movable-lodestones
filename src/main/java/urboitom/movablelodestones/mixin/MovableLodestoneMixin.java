6fcfc7046fa13354c62f336cc141d49dbaceccf8
Sorro123 <blackassassin2416@gmail.com>
2026-03-13 16:06:07 +0100

    Move from yarn to MojMap + parchment

--- src/main/java/urboitom/movablelodestones/mixin/MovableLodestoneMixin.java
+++ src/main/java/urboitom/movablelodestones/mixin/MovableLodestoneMixin.java
@@ -1,26 +1,26 @@
 package urboitom.movablelodestones.mixin;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.PushReaction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(AbstractBlock.AbstractBlockState.class)
+@Mixin(BlockBehaviour.BlockStateBase.class)
 public abstract class MovableLodestoneMixin {
 
 	// Inject our code at the very top (HEAD) of the getPistonBehavior method
-	@Inject(method = "getPistonBehavior", at = @At("HEAD"), cancellable = true)
-	private void makeLodestoneMovable(CallbackInfoReturnable<PistonBehavior> cir) {
+	@Inject(method = "getPistonPushReaction", at = @At("HEAD"), cancellable = true)
+	private void makeLodestoneMovable(CallbackInfoReturnable<PushReaction> cir) {
 
 		// Treat 'this' as the block state we are querying
-		AbstractBlock.AbstractBlockState state = (AbstractBlock.AbstractBlockState) (Object) this;
+		BlockBehaviour.BlockStateBase state = (BlockBehaviour.BlockStateBase) (Object) this;
 
 		// If the block is a Lodestone, force the behavior to NORMAL
-		if (state.isOf(Blocks.LODESTONE)) {
-			cir.setReturnValue(PistonBehavior.NORMAL);
+		if (state.is(Blocks.LODESTONE)) {
+			cir.setReturnValue(PushReaction.NORMAL);
 		}
 	}
 }
